337c0ad92d2829afa9f60218ad228dffca114045
bluewebgeek <BLUE.WEB.GEEK@gmail.com>
2019-01-14 15:41:39 +0800

    Add new rust source for binary turn into library

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 extern crate libc;
 
-#[no_mangle]
-pub extern fn hello_world() {
-    println!("Hello World!");
-}
+
+pub mod benchcaster_main;
+pub mod benchmarker_main;
+pub mod coincaster_main;
+pub mod fullnode_config_main;
+pub mod fullnode_main;
+pub mod genesis_main;
+pub mod keygen_main;
+pub mod ledgertool_main;
+pub mod propagator_main;
+pub mod upload_enhancer_main;
+pub mod wallet_main;
+pub mod errors;
