7ce7262ba36a34309d5568ecfdf7e54911cb5b3f
bluewebgeek <BLUE.WEB.GEEK@gmail.com>
2019-02-11 09:12:17 +0800

    Add try macro for ffi

--- src/macros.rs
+++ src/macros.rs
@@ -1,15 +1,18 @@
 #[macro_export]
+
+
 macro_rules! try_ffi {
     ($expr:expr) => {
         match $expr {
             Ok(expr) => expr,
             Err(err) => {
                 //crate::rustelo_error::ERROR
-                 //   .lock()
-                 //   .replace(failure::Error::from(err));
-
+                //    .lock()
+                //    .replace(failure::Error::from(err));
+                println!("Expr error when running {:?}", err);
                 return crate::rustelo_error::RusteloResult::Failure;
             }
+            
         }
     };
 }
